bc4d0f5dcc75c861ca29b367b64fdb77ac1def78
egoreu62-ship-it <egoreu62@gmail.com>
2026-08-17 12:47:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
+        GatewayIntentBits.MessageContent,
         GatewayIntentBits.GuildVoiceStates
     ]
 });
