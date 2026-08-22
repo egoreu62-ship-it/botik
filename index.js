934e331273aafab61e6867a43d9cf6cd8a8387a1
egoreu62-ship-it <egoreu62@gmail.com>
2026-08-23 04:43:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1190,7 +1190,7 @@ startTurnTimer(); // запускаем таймер на самый первы�
         let winnings, resultText;
 const houseEdgeRoll = Math.random(); // шанс "дом выигрывает" независимо от символов
 
-if (houseEdgeRoll < 0.65) {
+if (houseEdgeRoll < 0.5) {
     // 65% случаев — гарантированный проигрыш, как в реальном казино
     winnings = -bet;
     resultText = `😔 Проигрыш: ${bet} 🪙`;
